06c1def3866d08e3fba50955b4aec53f9a8fb22f
damola <dbamijoko@yahoo.com>
2025-05-13 12:09:38 +0100

    new tests

--- cypress/e2e/saucedemoTests/login.cy.js
+++ cypress/e2e/saucedemoTests/login.cy.js
@@ -1,13 +1,24 @@
 describe("Login functionality tests", () => {
-    beforeEach(() => {
-        cy.visit("https://www.saucedemo.com/");
-    })
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com/");
+  });
 
-    it("Valid login", () => {
-        cy.get("#user-name").type("standard_user");
-        cy.get("#password").type("secret_sauce");
-        cy.get("#login-button").click();
-        cy.url().should("include", "/inventory.html");
-        cy.get(".title").should("contain.text", "Products");
-    })
-})
+  it("should validate successful login and redirect to inventory page", () => {
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauce");
+    cy.get("#login-button").click();
+    cy.url().should("include", "/inventory.html");
+    cy.get(".title").should("contain.text", "Products");
+  });
+
+  it("should show error message for invalid password", () => {
+    cy.get("#user-name").type("standard_user");
+    cy.get("#password").type("secret_sauc"); // wrong password
+    cy.get("#login-button").click();
+    cy.get('[data-test="error"]').should(
+      "have.text",
+      "Epic sadface: Username and password do not match any user in this service"
+    );
+  });
+
+});
